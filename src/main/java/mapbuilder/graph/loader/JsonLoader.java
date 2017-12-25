b29c74a07d252f948ced9c451d382e70aaa738a5
BoseSean <xinyesg@gmail.com>
2017-12-25 17:56:57 +0800

    added load pointMap, data

--- src/main/java/mapbuilder/graph/loader/JsonLoader.java
+++ src/main/java/mapbuilder/graph/loader/JsonLoader.java
@@ -12,14 +12,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JsonLoader {
     public static List<Edge2D> loadEdges(String file) throws IOException, JSONException {
         throw new NotImplementedException();
     }
 
-    public static List<Vector2D> loadNodes(String file) throws IOException, JSONException {
+    public static List<Vector2D> loadNodesList(String file) throws IOException, JSONException {
         String content = new String(Files.readAllBytes(Paths.get(file)));
 
         JSONObject jobj = new JSONObject(content);
@@ -33,6 +35,23 @@ public class JsonLoader {
             pointSet.add(vec);
         }
         return pointSet;
+    }
+    public static Map<String,Vector2D> loadNodesMap(String file) throws IOException, JSONException {
+        String content = new String(Files.readAllBytes(Paths.get(file)));
+
+        JSONObject jobj = new JSONObject(content);
+        Map<String,Vector2D> pointMap = new HashMap<>();
+        JSONArray arr = jobj.getJSONObject("main/java/mapbuilder/graph").getJSONArray("nodes");
+        JSONObject node;
+        for (int i = 0; i < arr.length(); i++) {
+            String id = arr.getJSONObject(i).getString("id");
+            node = arr.getJSONObject(i).getJSONObject("meta-data");
+
+            Node vec = new Node(node.getInt("x"), node.getInt("y"));
+            vec.setId(id);
+            pointMap.put(id ,vec);
+        }
+        return pointMap;
 
     }
 
