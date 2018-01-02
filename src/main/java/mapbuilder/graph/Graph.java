059f93e13e1b0f4bb5c02d2e7a48ad26908a6476
BoseSean <xinyesg@gmail.com>
2018-01-02 23:40:49 +0800

    made edges bidirectional

--- src/main/java/mapbuilder/graph/Graph.java
+++ src/main/java/mapbuilder/graph/Graph.java
@@ -1,5 +1,32 @@
 package mapbuilder.graph;
 
-public interface Graph {
+import java.util.List;
+
+public class Graph {
+    public Graph(List<Edge2D> edges, List<Vector2D> nodes) {
+        this.edges = edges;
+        this.nodes = nodes;
+    }
+
+    public List<Edge2D> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Edge2D> edges) {
+        this.edges = edges;
+    }
+
+    public List<Vector2D> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<Vector2D> nodes) {
+        this.nodes = nodes;
+    }
+
+    private List<Edge2D> edges;
+    private List<Vector2D> nodes;
+
+
 
 }
