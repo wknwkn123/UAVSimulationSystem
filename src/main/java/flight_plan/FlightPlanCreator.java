ca4bdfae76c692521e4430072ed93a09fcf3a264
Ziji Shi <shi.ziji.sm@gmail.com>
2018-01-24 17:06:15 +0800

    Stable version found.

--- src/main/java/flight_plan/FlightPlanCreator.java
+++
@@ -1,8 +0,0 @@
-package flight_plan;
-import airspaceengine.airspacestructure.AirspaceStructure;
-
-import java.util.List;
-
-public interface FlightPlanCreator {
-    List<FlightPlan> createFlightPlans(AirspaceStructure airMap);
-}
