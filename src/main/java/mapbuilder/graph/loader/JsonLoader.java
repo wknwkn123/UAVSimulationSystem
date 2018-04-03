5004eba5e4afdc82087ed9d096ea8d052edffda6
ekoeditaa <ekoeditaa@gmail.com>
2018-04-04 00:29:23 +0800

    add airspace server

--- src/main/java/mapbuilder/graph/loader/JsonLoader.java
+++ src/main/java/mapbuilder/graph/loader/JsonLoader.java
@@ -2,17 +2,20 @@ package mapbuilder.graph.loader;
 
 import mapbuilder.graph.Edge2D;
 import mapbuilder.graph.Vector2D;
+import mapbuilder.triangulation.Node;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-import mapbuilder.triangulation.Node;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 public class JsonLoader {
     public List<Edge2D> loadEdges(String file) throws IOException, JSONException {
@@ -35,6 +38,29 @@ public class JsonLoader {
         return pointSet;
     }
 
+    public Vector<Vector2D> loadNodeListWithString(String jsonString) {
+
+        JSONObject jobj = new JSONObject(jsonString);
+        Vector<Vector2D> pointSet = new Vector<>();
+        JSONArray arr = jobj.getJSONObject("graph").getJSONArray("nodes");
+        JSONObject node;
+        for (int i = 0; i < arr.length(); i++) {
+            node = arr.getJSONObject(i).getJSONObject("meta-data");
+            Node vec = new Node(node.getInt("x"), node.getInt("y"));
+            vec.setId(arr.getJSONObject(i).getString("id"));
+            pointSet.add(vec);
+        }
+        return pointSet;
+    }
+
+    public String writeEdges2JsonString(String jsonString, JSONArray jsonEdges) throws IOException {
+
+        JSONObject jobj = new JSONObject(jsonString);
+        jobj.getJSONObject("graph").put("edges", jsonEdges);
+        return jobj.toString();
+
+
+    }
     public Map<String,Vector2D> loadNodesMap(String file) throws IOException, JSONException {
         String content = new String(Files.readAllBytes(Paths.get(file)));
 
--- src/main/java/simulationengine/CoreEngine.java
+++ src/main/java/simulationengine/CoreEngine.java
@@ -1,20 +1,7 @@
 package simulationengine;
 
-import airspaceengine.AirspaceEngine;
-import collisionavoidanceengine.FlightPlanScheduler;
-import collisionavoidanceengine.flightplan.Flight;
-import config.Config;
-import flight_plan.FlightPlanEngine;
-import uav.UAVEngine;
+import websocket.simple_v2.server.HTTPServer;
 import websocket.simple_v2.server.Websocket;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Entry point for the application.
@@ -22,54 +9,11 @@ import java.util.concurrent.TimeUnit;
 
 public class CoreEngine {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //start websocket server
         Websocket.getInstance().startServer();
-//
-//        try {
-//            TimeUnit.MILLISECONDS.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        //create airspace
-//        AirspaceEngine.getInstance().createAirspace(SimulationConfiguration.getInstance().getConfig());
-//
-//        //create UAVs
-//        UAVEngine.getInstance().createUAVs("RANDOM");
-//
-//        //create schedule/demand
-//        FlightPlanEngine.getInstance().getFlights().add(new Flight("FL_00001", "RQ_001", "UV_000001", 0, 5));
-//        FlightPlanEngine.getInstance().getFlights().add(new Flight("FL_00002", "RQ_002", "UV_000002", 2, 6));
-//        FlightPlanEngine.getInstance().getFlights().add(new Flight("FL_00003", "RQ_003", "UV_000003", 4, 10));
-//        ArrayList<String> path1 = new ArrayList<>();
-//        path1.add("WP_B85");
-//        path1.add("WP_B97");
-//        FlightPlanEngine.getInstance().getFlights().get(0).setFlightPath(path1);
-//        ArrayList<String> path2 = new ArrayList<>();
-//        path2.add("WP_THM");
-//        path2.add("WP_GMLM");
-//        FlightPlanEngine.getInstance().getFlights().get(1).setFlightPath(path2);
-//        ArrayList<String> path3 = new ArrayList<>();
-//        path2.add("WP_Y61M");
-//        path2.add("WP_Y28M");
-//        FlightPlanEngine.getInstance().getFlights().get(2).setFlightPath(path2);
-//        FlightPlanEngine.getInstance().createFlightPlans(SimulationConfiguration.getInstance().getFlightScheduleType(), AirspaceEngine.getInstance().getAirMap());
 
-//        FlightPlanScheduler scheduler = new FlightPlanScheduler(new Config());
-//        scheduler.ScheduleFlight();
-
-//        for (Map.Entry<String, Flight> entry : scheduler.currentFlightPlan.getFlightPlan().entrySet())
-//        {
-//            System.out.println(entry.getKey() + "/" + entry.getValue());
-//        }
-
-//        //assign schedule to UAVs
-//        FlightPlanEngine.getInstance().assignFlightPlans("RANDOMPLAN");
-//
-//        //run simulation
-//        UAVEngine.getInstance().startThread();
-//        Thread t = new Thread(SimulationApp.getInstance());
-//        t.start();
+        //start http-server for airspace editor
+        HTTPServer.getInstance().startServer();
     }
 }
